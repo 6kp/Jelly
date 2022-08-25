b2afe4f9c22fc473544d0206ecfa551aaeb322be
6kp <m7g3@hotmail.com>
2022-08-25 02:48:33 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jelly
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Main());
+        }
+
+
+
+        public static string AppName = "Jelly"; //Change this to your desired app name
+        public static string BuildVer = "1.0.0"; //Change this to the current build version
+
+        public static void getVer()
+        {
+            WebClient wc = new WebClient();
+            var ver = wc.DownloadString("https://pastebin.com/raw/Dxv5RbWN"); //Make a Pastebin document containing your latest version
+
+            if (ver.Contains(BuildVer))
+            {
+                return;
+            }
+            else
+            {
+                MessageBox.Show("You don't have the latest version!", AppName);
+                Environment.Exit(0);
+            }
+        }
+    }
+}
